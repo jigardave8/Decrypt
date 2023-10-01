ed8eed246e22fbd87822478be88db6a84c80e558
Jay D <jdavenz8@gmail.com>
2023-10-01 15:40:06 +1300

    Initial Commit

---
+++ Decrypt/DecryptApp.swift
@@ -0,0 +1,17 @@
+//
+//  DecryptApp.swift
+//  Decrypt
+//
+//  Created by Jigar on 01/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DecryptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
